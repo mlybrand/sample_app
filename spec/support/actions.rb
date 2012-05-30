1ddaaef9f4fa7d9928ee727b2f269db5152adf16
Mark Lybrand <mlybrand@gmail.com>
2012-05-30 13:55:11 -0700

    Finish user edit, update, index and destroy actions

--- spec/support/actions.rb
+++ spec/support/actions.rb
@@ -1,7 +1,9 @@
-def valid_signin(user)
+def fake_sign_in(user)
 	fill_in "Email", with: user.email
 	fill_in "Password", with: user.password
 	click_button "Sign in"
+	# Sign in when not using Capybara as well.
+	cookies[:remember_token] = user.remember_token
 end
 
 def go_home
